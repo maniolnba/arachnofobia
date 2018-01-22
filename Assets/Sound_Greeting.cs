b60273e66fc54f5708004a93f71d3a14544161bb
maniolnba <tomasz.smolinski@poczta.pl>
2018-01-22 21:23:17 +0100

    safety_commit

---
+++ Assets/Sound_Greeting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sound_Greeting : MonoBehaviour {
+    public AudioSource nagranie_powitalne;
+    public int counter = 0;
+    private GameObject spider;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            counter++;
+            if (counter == 1) {
+                nagranie_powitalne.Play();
+
+            }
+            else if (counter == 2)
+            {
+                SceneManager.LoadScene("safetyRoom");
+            }
+            else if (counter == 3)
+            {
+                 
+            }
+        }
+	}
+}
---
+++ Assets/showSpider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showSpider : MonoBehaviour {
+	public GameObject spider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Z)){
+		spider.SetActive (false);
+	}
+	else if (Input.GetKeyDown(KeyCode.A)){
+			spider.SetActive (true);
+}
+
+}
+}
